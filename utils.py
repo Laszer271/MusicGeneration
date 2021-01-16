f0f26cd0b1a95b394766190c704ceef94077571a
Naichiri <naichiri@gmail.com>
2021-01-16 23:14:27 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+OCTAVE_LENGTH = 12
+MINOR_SECOND_INTERVAL = 1
+MAJOR_SECOND_INTERVAL = 2
+TRITONE_INTERVAL = 6
+MINOR_SEVENTH_INTERVAL = 10
+MAJOR_SEVENTH_INTERVAL = 11
+DISSONANCES = [MINOR_SECOND_INTERVAL, MAJOR_SECOND_INTERVAL, TRITONE_INTERVAL, MINOR_SEVENTH_INTERVAL, MAJOR_SEVENTH_INTERVAL]
+
 POSSIBLE_VALUES = range(-2, 128)
 PROLONGATION_VALUE = -2
 REST_VALUE = -1
@@ -7,7 +15,6 @@ NOTE_VALUES = range(0, 128)
 
 LOWEST_PITCH = 0 # has to be C for proper key defining
 HIGHEST_PITCH = 127
-OCTAVE_LENGTH = 12
 FULL_OCTAVES_NO = int(np.floor(HIGHEST_PITCH / OCTAVE_LENGTH))
 
 PROLONGATION_WEIGHT = 0.5
@@ -19,3 +26,7 @@ def generate_music_vector(n):
     while song[0] == -2:
         song[0] = np.random.choice(POSSIBLE_VALUES, p=WEIGHTS)
     return song
+
+def get_intervals(gen_vec):
+    notes = gen_vec[gen_vec >= LOWEST_PITCH]
+    return np.abs(notes[1:] - notes[:-1])
