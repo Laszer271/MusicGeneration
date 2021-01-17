3fadcaf72cb5ac5a7406ba4a23049776cd9a14fa
Naichiri <naichiri@gmail.com>
2021-01-17 12:11:15 +0100

    statistical fitness function

--- features.py
+++ features.py
@@ -3,14 +3,14 @@ import representations as rep
 import tonality
 import utils
 
-# rule-based features
-def mean_pitch(gen_vec):
+# statistical features
+def calculate_mean_pitch(gen_vec, _):
     mean = np.average(gen_vec[gen_vec >= utils.LOWEST_PITCH])
     return mean / utils.HIGHEST_PITCH
-def pitch_deviation(gen_vec):
+def calculate_pitch_deviation(gen_vec, _):
     deviation = np.std(gen_vec[gen_vec >= utils.LOWEST_PITCH])
     return deviation / utils.HIGHEST_PITCH
-def off_scale_notes(gen_vec, music_key):
+def calculate_off_scale_notes(gen_vec, music_key):
     off_scale_notes_no = np.count_nonzero(np.isin(gen_vec[gen_vec >= utils.LOWEST_PITCH], music_key) == False)
     notes_no = np.count_nonzero(gen_vec[gen_vec >= utils.LOWEST_PITCH])
     if notes_no == 0:
@@ -22,33 +22,49 @@ def get_chord_pitches(gen_vec):
 def get_dissonances(gen_vec):
     return
 '''
-def minor_and_major_seconds(gen_vec):
+def calculate_minor_and_major_seconds(gen_vec, _):
     intervals = utils.get_intervals(gen_vec)
     if intervals.size == 0:
         return 0
     seconds_no = np.count_nonzero(np.isin(intervals, np.array([utils.MINOR_SECOND_INTERVAL, utils.MAJOR_SECOND_INTERVAL])))
     return seconds_no / np.count_nonzero(intervals)
-def intervals_larger_than_octave(gen_vec):
+def calculate_intervals_larger_than_octave(gen_vec, _):
     intervals = utils.get_intervals(gen_vec)
     if intervals.size == 0:
         return 0
     intervals_larger_than_octave_no = np.count_nonzero(intervals > utils.OCTAVE_LENGTH)
     return intervals_larger_than_octave_no / np.count_nonzero(intervals)
-def get_mean_rhythmic_value(gen_vec):
-    #TODO
-    return np.average(rep.gen_to_midi(gen_vec)[1])
-def get_rhythm_deviation(gen_vec):
-    #TODO
-    return np.std(rep.gen_to_midi(gen_vec)[1])
-def notes_in_strong_beat(gen_vec):
+def calculate_mean_rhythmic_value(gen_vec, _):
+    mean = np.average( np.log( np.array(rep.gen_to_midi(gen_vec)[1]) / 4 + 1 ) )
+    whole_notes = np.log(5)
+    return mean / whole_notes
+def calculate_rhythm_deviation(gen_vec, _):
+    deviation = np.std( np.log( np.array(rep.gen_to_midi(gen_vec)[1]) / 4 + 1 ) )
+    whole_notes = np.log(5)
+    return deviation / whole_notes
+def calculate_notes_in_strong_beat(gen_vec, _):
     strong_beats = gen_vec[0::8]
     if strong_beats.size == 0:
         return 1
     notes_in_strong_beat_no = np.count_nonzero(strong_beats >= utils.LOWEST_PITCH)
     return notes_in_strong_beat_no / strong_beats.size
-def rests_to_notes_ratio(gen_vec):
+def calculate_rests_to_notes_ratio(gen_vec, _):
     rhythmical_no = np.count_nonzero(gen_vec >= utils.REST_VALUE)
     if rhythmical_no == 0:
         return 0
     rests_no = np.count_nonzero(gen_vec == utils.REST_VALUE)
     return rests_no / rhythmical_no
+
+# fitness function, weight, mean value, standard deviation
+mean_pitch = (calculate_mean_pitch, 1, 0.564, 0.065)
+pitch_deviation = (calculate_pitch_deviation, 1, 0.053, 0.013)
+off_scale_notes = (calculate_off_scale_notes, 1, 0, 0.3)
+minor_and_major_seconds = (calculate_minor_and_major_seconds, 1, 0.553, 0.078)
+intervals_larger_than_octave = (calculate_intervals_larger_than_octave, 1, 0.007, 0.010)
+mean_rhythmic_value = (calculate_mean_rhythmic_value, 1, 0.282, 0.12)
+rhythm_deviation = (calculate_rhythm_deviation, 1, 0.156, 0.058)
+notes_in_strong_beat = (calculate_notes_in_strong_beat, 1, 0.788, 0.218)
+rests_to_notes_ratio = (calculate_rests_to_notes_ratio, 1, 0.105, 0.092)
+
+statistical_features = np.array([mean_pitch, pitch_deviation, off_scale_notes, minor_and_major_seconds, intervals_larger_than_octave,
+                                 mean_rhythmic_value, rhythm_deviation, notes_in_strong_beat, rests_to_notes_ratio])
